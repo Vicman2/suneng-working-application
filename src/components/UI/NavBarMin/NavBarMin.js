aee5cab0051ce59a5fa7d6dbcd80b8886c4abd58
Vicman2 <vicmanthebest@gmail.com>
2020-01-28 15:49:54 +0000

    added some link for mobile navigation

--- src/components/UI/NavBarMin/NavBarMin.js
+++ src/components/UI/NavBarMin/NavBarMin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 import './NavBarMin.css'
 
 const droplet = (props) => {
@@ -14,11 +15,12 @@ const droplet = (props) => {
         <div className={classes.join(" ")}>
             <div className="LogoMe">LOGO</div>
             <nav className="NavBarItems">
-                <div><a href="/">Home</a></div>
-                <div><a href="/">Product</a></div>
-                <div><a href="/">Services</a></div>
-                <div><a href="/">About Us</a></div>
-                <div><a href="/">Contact</a></div>
+                <div><NavLink activeClassName="active" to="/"> Home </NavLink></div>
+                <div><NavLink activeClassName="active" to="/products"> Products</NavLink></div>
+                <div><NavLink activeClassName="active" to="/services"> Services </NavLink></div>
+                <div><NavLink activeClassName="active" to="/about"> About </NavLink></div>
+                <div><NavLink activeClassName="active" to="/contact"> Contact Us</NavLink></div>
+                <div><NavLink activeClassName="active" to="/orders"> Orders</NavLink></div>
             </nav>
         </div>
     )
